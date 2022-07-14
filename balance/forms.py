83a1dc72d0bf6442d13a82d7613c6d190e6a5f5c
Luisda <lsanchezroldan@gmail.com>
2022-07-14 18:22:39 +0200

    agregamos validaciones a todos los campos

--- balance/forms.py
+++ balance/forms.py
@@ -6,8 +6,16 @@ class MovimientosForm(FlaskForm):
     id = HiddenField()
     fecha = DateField("Fecha", validators=[DataRequired(
         message="Debes introducir una fecha")])
-    concepto = StringField("Concepto", validators=[DataRequired()])
-    tipo = RadioField(choices=[("I", "Ingreso"), ("G", "Gasto")])
-    cantidad = FloatField("Cantidad")
+    concepto = StringField(
+        "Concepto", validators=[
+            DataRequired("Debes escribir un concepto"),
+            Length(min=3, max=25, message="Debe tener entre 3 y 25 caracteres")
+        ])
+    tipo = RadioField(
+        choices=[("I", "Ingreso"), ("G", "Gasto")],
+        validators=[DataRequired(message="¿Es un gasto o un ingreso?")]
+    )
+    cantidad = FloatField("Cantidad", validators=[
+                          DataRequired(message="Debes especificar un número")])
 
     submit = SubmitField("Guardar", render_kw={ "class": "blue-button" })
